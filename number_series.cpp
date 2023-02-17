5190aaebb7b1b8f7f305d3b725f1be59a79d6c64
Marius Mikučionis <marius@cs.aau.dk>
2023-02-17 07:04:50 +0100

    Initial template for the extended exercise 1

---
+++ number_series.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definitions/implementation of some number_series methods
+ */
+
+#include "number_series.h"
+
+#include <algorithm>
+#include <iterator>
+#include <random>
+#include <functional>
+
+namespace data_series
+{
+    number_series::number_series() {}
+
+    number_series_wrap::number_series_wrap() {}
+}  // namespace data_series
